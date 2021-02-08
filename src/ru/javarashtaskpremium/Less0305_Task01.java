fd0484688acb4b38677db98f63f34313c4e8e461
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 18:41:56 +0400

    Задача Java Syntax New,  3 уровень,  5 лекция. Как зовут кота?

---
+++ src/ru/javarashtaskpremium/Less0305_Task01.java
@@ -0,0 +1,30 @@
+package ru.javarashtaskpremium;
+
+public class Less0305_Task01 {
+    /* Помоги коту обрести имя с помощью метода setName.
+
+Требования:
+•	Класс Cat должен содержать только одну переменную name.
+•	Переменная name должна иметь модификатор доступа private и тип String.
+•	Класс Cat должен содержать только два метода - setName и main.
+•	Метод setName класса Cat должен устанавливать значение переменной private String name
+равным переданному параметру String name. */
+
+    /*
+    private String name = "безымянный кот";
+
+    public void setName(String name) {
+        this.name = name; // Добавил this.
+
+    }
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.setName("Жужик");
+        System.out.println(cat.name);
+    }
+    */
+
+}
+// закоментировал. в данном случае код компилироваться не будет. имя класса должно быть Cat
+
