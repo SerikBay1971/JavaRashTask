24d778a7a3bf1029f04bf296747eb0508aa21988
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 16:08:17 +0400

    Задача Java Syntax New,  1 уровень,  4 лекция Объявление переменных

---
+++ src/ru/javarashtaskpremium/Less0104_Task03.java
@@ -0,0 +1,19 @@
+package ru.javarashtaskpremium;public class Less0104_Task03 {
+/* Напиши программу, которая в методе main объявляет такие переменные: name типа String, age типа int и city типа String.
+
+Примечание: "объявить переменную" значит то же, что и "создать переменную".
+
+Требования:
+•	Объяви переменную name типа String.
+•	Объяви переменную age типа int.
+•	Объяви переменную city типа String.
+•	Должны быть объявлены 3 переменные. */
+
+    public static void main(String[] args)
+    {
+        //напишите тут ваш код
+        String name;
+        int age;
+        String city;
+    }
+}
