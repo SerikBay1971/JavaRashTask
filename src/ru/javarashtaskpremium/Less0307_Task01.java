4476db812751e95b3a35fcbec4b1dd31b4b42d1d
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 18:44:35 +0400

    Задача Java Syntax New,  3 уровень,  7 лекция. Хорошо или плохо?

---
+++ src/ru/javarashtaskpremium/Less0307_Task01.java
@@ -0,0 +1,33 @@
+package ru.javarashtaskpremium;
+
+public class Less0307_Task01 {
+    /* Напиши метод compare(int a), чтобы он:
+
+    выводил на экран строку "Число меньше 5", если параметр метода меньше 5,
+    выводил строку "Число больше 5", если параметр метода больше 5,
+    выводил строку "Число равно 5", если параметр метода равен 5.
+    Требования:
+            •	Программа должна выводить текст на экран.
+            •	Метод main не должен вызывать System.out.println или System.out.print.
+•	Метод main должен вызывать метод compare.
+            •	Метод compare должен быть void.
+            •	Метод compare должен выводить текст на экран согласно заданию. */
+    public static void main(String[] args) {
+        compare(3);
+        compare(6);
+        compare(5);
+    }
+
+    public static void compare(int a) {
+        if (a < 5)
+            System.out.println("Число меньше 5");
+        else {
+            if (a > 5)
+                System.out.println("Число больше 5");
+            else
+                System.out.println("Число равно 5");
+        }
+
+
+    }
+}
