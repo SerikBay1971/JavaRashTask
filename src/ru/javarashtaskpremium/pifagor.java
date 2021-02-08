daa56df6d66cf8d13e51c37e6e4745c4566a209e
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 19:16:59 +0400

    Вариант через цикл for

--- src/ru/javarashtaskpremium/pifagor.java
+++ src/ru/javarashtaskpremium/pifagor.java
@@ -3,20 +3,16 @@ package ru.javarashtaskpremium;
 public class pifagor {
     public static void main(String[] args) {
         int z;
-        int y = 1;
-        while (y <= 9) {
-            int x = 1;
+        for (int y = 1; y <= 9; y++) {
             if (y == 2) System.out.println("  ---------------------------");
-            while (x <= 9) {
+            for (int x = 1; x <= 9; x++) {
                 z = x * y;
                 if (x == 2) System.out.print(" |");
                 if (z == 1) System.out.print("   ");
                 if (z > 1 && z < 10) System.out.print("  " + z);
                 if (z >= 10 && z < 100) System.out.print(" " + z);
-                x++;
             }
             System.out.println();
-            y++;
         }
     }
 }
