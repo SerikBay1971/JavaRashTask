1b74d72ce0e51258a704cb03f9aa4518c2f2d4e8
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 16:59:48 +0400

    Задача Java Syntax New,  2 уровень,  4 лекция. Hello Amigo!

---
+++ src/ru/javarashtaskpremium/Less0204_Task01.java
@@ -0,0 +1,17 @@
+package ru.javarashtaskpremium;
+
+public class Less0204_Task01 {
+    /* Внеси изменения в программу, чтобы переменная name имела значение Amigo.
+
+Требования:
+•	Переменной name должно присваиваться значение сразу после создания.
+•	Должно быть объявлено 2 переменных.
+•	Значение переменной text изменять нельзя.
+•	Команду вывода текста на экран изменять нельзя.
+•	Программа должна выводить текст "Hello, Amigo!". */
+    public static void main(String[] args) {
+        String name = "Amigo";
+        String text = "Hello, " + name + "!";
+        System.out.println(text);
+    }
+}
