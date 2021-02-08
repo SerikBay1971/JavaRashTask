226413384fc09acfa4328907bd0b3dd10a4641a5
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 18:42:24 +0400

    Задача Java Syntax New,  3 уровень,  5 лекция. Учет котов

---
+++ src/ru/javarashtaskpremium/Less0305_Task02.java
@@ -0,0 +1,27 @@
+package ru.javarashtaskpremium;
+
+public class Less0305_Task02 {
+    /* Напиши код в методе addNewCat, чтобы при его вызове количество котов увеличивалось на 1.
+За количество котов отвечает переменная catsCount.
+
+Требования:
+•	Класс Cat должен содержать только одну переменную catsCount.
+•	Переменная catsCount должна быть статической, иметь модификатор доступа private, тип int и проинициализирована нулем.
+•	Класс Cat должен содержать два метода addNewCat и main.
+•	Метод addNewCat класса Cat должен увеличивать количество котов на 1. */
+
+    /*
+    private static int catsCount = 0;
+
+    public static void addNewCat() {
+        Cat.catsCount = Cat.catsCount + 1; // Мой код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+    */
+}
+
+// в данном случае компилироваться не будет. имя класса должно быть Cat
