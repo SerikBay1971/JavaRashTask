a80de34f8fca2174bf52afd52f4258741f48f449
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 18:32:03 +0400

    Задача Java Syntax New,  2 уровень,  4 лекция. Исправь программу

---
+++ src/ru/javarashtaskpremium/Less0204_Task02.java
@@ -0,0 +1,17 @@
+package ru.javarashtaskpremium;
+
+public class Less0204_Task02 {
+    /* Внеси изменения в программу, чтобы на экран выводился текст: "Coding in java". Используй переменные.
+
+Требования:
+•	Должно быть объявлено 3 переменных.
+•	Всем переменным должны быть присвоены значения сразу после создания.
+•	Команду вывода текста на экран изменять нельзя.
+•	Программа должна выводить текст "Coding in java". */
+    public static void main(String[] args) {
+        String string1 = "Coding";
+        String string2 = "in";
+        String string3 = "java";
+        System.out.println(string1 + " " + string2 + " " + string3);
+    }
+}
