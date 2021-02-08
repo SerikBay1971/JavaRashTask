f1880518bfe4fd8a0604dbba0755119100449d88
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 18:42:52 +0400

    Задача Java Syntax New,  3 уровень,  5 лекция. Задаем количество котов

---
+++ src/ru/javarashtaskpremium/Less0305_Task03.java
@@ -0,0 +1,25 @@
+package ru.javarashtaskpremium;
+
+public class Less0305_Task03 {
+    /* Напиши метод setCatsCount. Метод должен устанавливать количество котов (catsCount).
+
+Требования:
+•	Класс Cat должен содержать только одну переменную catsCount.
+•	Переменная catsCount должна быть статической, иметь модификатор доступа private, тип int и проинициализирована нулем.
+•	Класс Cat должен содержать два метода setCatsCount и main.
+•	Метод setCatsCount класса Cat должен устанавливать значение переменной catsCount равным переданному параметру. */
+
+    /*    private static int catsCount = 0;
+
+    public static void setCatsCount(int catsCount) {
+        Cat.catsCount = catsCount; // Мой код
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+    */
+}
+
+// в данном случае компилироваться не будет. имя класса должно быть Cat
