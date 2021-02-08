66a95f7b3e1b0397de2695b541f6bd92e7211df0
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 18:43:29 +0400

    Задача Java Syntax New,  3 уровень,  5 лекция. Программа учета имен

---
+++ src/ru/javarashtaskpremium/Less0305_Task04.java
@@ -0,0 +1,29 @@
+package ru.javarashtaskpremium;
+
+public class Less0305_Task04 {
+    /* Допиши код метода setName,
+    чтобы с его помощью установить значение private String fullName, равное значению локальной переменной String fullName.
+
+Требования:
+•	Класс Cat должен содержать только одну переменную fullName.
+•	Переменная fullName должна иметь модификатор доступа private и тип String.
+•	Класс Cat должен содержать только два метода - setName и main.
+•	Метод setName должен иметь локальную переменную fullName.
+•	Метод setName класса Cat должен устанавливать значение глобальной переменной private String fullName
+равным локальной переменной fullName. */
+
+
+    /*    private String fullName;
+
+    public void setName(String firstName, String lastName) {
+        String fullName = firstName + " " + lastName;
+
+        this.fullName = fullName; // мой код
+    }
+
+    public static void main(String[] args) {
+
+    }
+    */
+}
+// в данном случае компилироваться не будет. имя класса должно быть Cat
