30efc05b72f4a2d2d94d74b92f861c70e0f867c1
Sergey Kolosov <serg.kolosov@gmail.com>
2021-02-08 14:07:57 +0400

    Задача Java Syntax New,  1 уровень,  4 лекция Комплимент учителю

---
+++ src/ru/javarashtaskpremium/Less0104_Task01.java
@@ -0,0 +1,42 @@
+package ru.javarashtaskpremium;
+
+public class Less0104_Task01 {
+    /* Написать программу, выводящую на экран надпись «Элли очень умная» 5 раз. Каждый раз – с новой строки.
+
+Пример вывода на экран:
+Элли очень умная
+Элли очень умная
+Элли очень умная
+Элли очень умная
+Элли очень умная
+
+Требования:
+•	Программа должна выводить текст.
+•	Текст должен начинаться с "Элли".
+•	Текст должен заканчиваться на "умная".
+•	Текст должен состоять из 5 строк.
+•	Выводимый текст должен соответствовать заданию. */
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        System.out.print("Элли ");
+        System.out.print("очень ");
+        System.out.println("умная");
+
+        System.out.print("Элли ");
+        System.out.print("очень ");
+        System.out.println("умная");
+
+        System.out.print("Элли ");
+        System.out.print("очень ");
+        System.out.println("умная");
+
+        System.out.print("Элли ");
+        System.out.print("очень ");
+        System.out.println("умная");
+
+        System.out.print("Элли ");
+        System.out.print("очень ");
+        System.out.println("умная");
+    }
+}
